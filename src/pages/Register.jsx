a52efd221754deca3fd247a5dca7699f9e52b2dd
edend85 <117260021+edend85@users.noreply.github.com>
2023-02-20 16:46:23 +0200

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,142 +4,126 @@ import User from '../classes/User';
 import { UserContext } from "../context/UserContext";
 
 const Register = () => {
-  const navigate = useNavigate();
-  const {CheckUserRegisterDetails} = useContext(UserContext);
+const navigate = useNavigate();
+const {
+  CheckUserRegisterDetails,
+  setUserName,
+  setPassword,
+  setPasswordAgain,
+  setImage,
+  setFirstName,
+  setLastName,
+  setEmail,
+  setDate,
+  setCity,
+  setStreet,
+  setStreetNum,
+  userName,
+  password,
+  passwordAgain,
+  image,
+  firstName,
+  lastName,
+  email,
+  date,
+  city,
+  street,
+  streetNum
 
-  // user details
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordAgain, setPasswordAgain] = useState("");
-  const [image, setImage] = useState(null);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [date, setDate] = useState("");
-  const [city, setCity] = useState("");
-  const [street, setStreet] = useState("");
-  const [streetNum, setStreetNum] = useState("");
+} = useContext(UserContext);
 
-  return (
-    <>
-      <h1>Register</h1>
-      <form className="register-form">
-        <label>
-          {" "}
-          UserName:
-          <input
-            type="text"
-            placeholder="UserName"
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Password:
-          <input
-            type="password"
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Password Again:
-          <input
-            type="password"
-            placeholder="password Again"
-            onChange={(e) => setPasswordAgain(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Upload Image:
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            onChange={(e) => setImage(URL.createObjectURL(e.target.files[0]))}
-          />
-        </label>
-        <label>
-          {" "}
-          First Name:
-          <input
-            type="text"
-            placeholder="first name"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Last Name:
-          <input
-            type="text"
-            placeholder="last name"
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Email:
-          <input
-            type="email"
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Date Of Birth:
-          <input type="date" onChange={(e) => setDate(e.target.value)} />
-        </label>
-        <label>
-          {" "}
-          City:
-          <input
-            type="text"
-            placeholder="city"
-            onChange={(e) => setCity(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Street:
-          <input
-            type="text"
-            placeholder="street"
-            onChange={(e) => setStreet(e.target.value)}
-          />
-        </label>
-        <label>
-          {" "}
-          Street Number:
-          <input
-            type="number"
-            placeholder="street number"
-            onChange={(e) => setStreetNum(e.target.value)}
-          />
-        </label>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            let newUser = new User(userName, password, image, firstName, lastName, email, date, city, street, streetNum);
-            CheckUserRegisterDetails(newUser, passwordAgain) ? navigate('/') : null;
-          }}
-        >
-          Create User
-        </button>
-      </form>
-      <div className="login">
-        <p>or</p>
-        <button
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Log In
-        </button>
-      </div>
-    </>
-  );
+// user details
+
+
+return (
+<>
+  <h1>Register</h1>
+  <form className="register-form">
+    <label>
+      {" "}
+      UserName:
+      <input type="text" placeholder="UserName" onChange={(e)=> setUserName(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Password:
+      <input type="password" placeholder="password" onChange={(e)=> setPassword(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Password Again:
+      <input type="password" placeholder="password Again" onChange={(e)=> setPasswordAgain(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Upload Image:
+      <input type="file" accept="image/png, image/jpeg" onChange={(e)=>
+      setImage(URL.createObjectURL(e.target.files[0]))}
+      />
+    </label>
+    <label>
+      {" "}
+      First Name:
+      <input type="text" placeholder="first name" onChange={(e)=> setFirstName(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Last Name:
+      <input type="text" placeholder="last name" onChange={(e)=> setLastName(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Email:
+      <input type="email" placeholder="email" onChange={(e)=> setEmail(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Date Of Birth:
+      <input type="date" onChange={(e)=> setDate(e.target.value)} />
+    </label>
+    <label>
+      {" "}
+      City:
+      <input type="text" placeholder="city" onChange={(e)=> setCity(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Street:
+      <input type="text" placeholder="street" onChange={(e)=> setStreet(e.target.value)}
+      />
+    </label>
+    <label>
+      {" "}
+      Street Number:
+      <input type="number" placeholder="street number" onChange={(e)=> setStreetNum(e.target.value)}
+      />
+    </label>
+    <button onClick={(e)=> {
+      e.preventDefault();
+      let newUser = new User(userName, password, image, firstName, lastName, email, date, city, street, streetNum);
+      CheckUserRegisterDetails(newUser, passwordAgain) ? navigate('/') : null;
+      }}
+      >
+      Create User
+    </button>
+  </form>
+  <div className="login">
+    <p>or</p>
+    <button onClick={()=> {
+      navigate("/");
+      }}
+      >
+      Log In
+    </button>
+  </div>
+</>
+);
 };
 export default Register;
