4414c7d8ae83f57641938ef7f35afd4b1b67b035
edend85 <117260021+edend85@users.noreply.github.com>
2023-02-20 16:46:25 +0200

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,44 @@
-const Profile = ()=>{
+import { useEffect } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
+
+const Profile = ()=>{ 
+
+    const navigate = useNavigate();
+    const {users} = useContext(UserContext);
+    const {userName} = useParams();
+    let user = users.find((u) => u.userName == userName);
+
+    const EditDetails =()=>{
+        navigate(`/editDetails/${userName}`);
+    }
+    const LogOut=()=>{
+        navigate("/");
+    }
+    const reportHazard=()=>{
+        console.log('report');
+    }
+ 
     return(
-        <h1>Profile page</h1>
+        user !== undefined ?
+        <>
+        <div id='profile'>
+          <div id='profileImage'><img src={user.image} style={{width:'10em'}}/></div>
+          <p>User Name: <span>{userName}</span></p>
+          <p>Full Name: <span>{user.firstName} {user.lastName}</span></p>
+          <p>Street Adress: <span>{user.street} {user.streetNum} {user.city}</span></p>
+          <p>Birth Date: <span>{user.date}</span></p>
+          <p>Email: <span>{user.email}</span></p>
+          <button id='edit' onClick={()=>EditDetails()}>Edit</button>
+          <button id='report' onClick={()=>reportHazard()}>Report</button>
+          <button id='delete' onClick={()=>LogOut()}>Log out</button>
+        </div>
+    </>
+    :
+    alert("bye")
     )
 }
 export default Profile;
