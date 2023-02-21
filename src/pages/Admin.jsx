0f5275710e4956f7d29fad77508377064ffd9f0b
edend85 <117260021+edend85@users.noreply.github.com>
2023-02-21 15:31:42 +0200

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,42 +1,51 @@
 import { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
-
 
 const Admin = ()=>{
 
-    const {users} = useContext(UserContext); 
-    const {userName} = useParams();
-    
-    return(
-        <>
-        <h1>Hi {userName} !</h1>
-        <table>
-            <thead style={{textAlign:"center"}}>
-                <tr>
+const {users,SetUsers} = useContext(UserContext);
+const navigate = useNavigate();
+
+
+return(
+<>
+    <h1>Hi !</h1>
+    <table>
+        <thead style={{textAlign:"center"}}>
+            <tr>
                 <th>Picture</th>
                 <th>User Name</th>
                 <th>Birth Date</th>
                 <th>Email</th>
                 <th>Address</th>
-                </tr>
-            </thead>
-            <tbody style={{textAlign:"center"}}>
-                { 
-                users.map((user,index)=>{
-                    return(
-                        <tr key={index}>
-                            <td><img src={user.image} style={{width:"5em"}}/></td>
-                            <td>{user.userName}</td>
-                            <td>{user.date}</td>
-                            <td>{user.email}</td>
-                            <td>{user.street} {user.streetNum} {user.city}</td>
-                        </tr> 
-                    )}
-                )}                
-            </tbody>
-        </table>
-        </>  
-    )
+            </tr>
+        </thead>
+        <tbody style={{textAlign:"center"}}>
+            {
+            users.map((user,index)=>{
+            return(
+            <tr key={index}>
+                <td><img src={user.image} style={{width:"5em"}} /></td>
+                <td>{user.userName}</td>
+                <td>{user.date}</td>
+                <td>{user.email}</td>
+                <td>{user.street} {user.streetNum} {user.city}</td>
+            </tr>
+            )}
+            )}
+        </tbody>
+    </table>
+    <div>
+    <button onClick={()=> {
+        navigate("/");
+        }}
+        >
+        Log In
+    </button>
+    </div>
+    
+</>
+)
 }
 export default Admin;
