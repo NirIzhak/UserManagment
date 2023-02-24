3caa399fc5884786881c355b057823d91d0342c8
edend85 <117260021+edend85@users.noreply.github.com>
2023-02-24 14:40:16 +0200

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,20 +1,15 @@
 import { useContext } from "react";
-import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import User from "../classes/User";
 import { UserContext } from "../context/UserContext";
 
 const Admin = ()=>{
 
 const {users,ReturnUser} = useContext(UserContext);
-const {userName} = useParams();
 const navigate = useNavigate();
+
 
 const UpdateUser=(event,userName)=>{
     event.preventDefault();
-   /* const nUser = new User(userName, password, image, firstName, lastName, email, date, city, street, streetNum);
-    users[userIndex] = nUser;
-    console.log('user :>> ', nUser);*/
     const user = ReturnUser(userName);
     console.log('object :>> ', user);
     navigate(`/editDetails/${user.userName}`);
@@ -25,12 +20,11 @@ const DeleteUser=(event,userName)=>{
     const user = ReturnUser(userName);
     let userIndex = users.indexOf(user);
     users.splice(userIndex,1);
-    console.log('users :>> ', users);
 }
 
 return(
 <>
-    <h1>Hi {userName} !</h1>
+    <h1>Hi admin !</h1>
     <table>
         <thead style={{textAlign:"center"}}>
             <tr>
@@ -51,10 +45,8 @@ return(
                 <td>{user.date}</td>
                 <td>{user.email}</td>
                 <td>{user.street} {user.streetNum} {user.city}</td>
-                <div>
-                <button onClick={(event)=>UpdateUser(event,user.userName)}>Edit</button>
-                <button onClick={(event)=>DeleteUser(event,user.userName)}>Delete</button>
-                </div>
+                <td><button onClick={(event)=>UpdateUser(event,user.userName)}>Edit</button></td> 
+                <td><button onClick={(event)=>DeleteUser(event,user.userName)}>Delete</button></td> 
             </tr>
             )}
             )}
