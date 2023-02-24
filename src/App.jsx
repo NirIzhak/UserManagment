1ed710fd1c144e1227a04af9a823cbcd4e21a505
edend85 <117260021+edend85@users.noreply.github.com>
2023-02-24 14:40:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Administrator from "./classes/Administrator";
-import UserContextProvider, { UserContext } from "./context/UserContext";
+import UserContextProvider from "./context/UserContext";
 import Admin from "./pages/Admin";
 import EditDetails from "./pages/EditDetails";
 import Login from "./pages/Login";
@@ -9,8 +8,7 @@ import Register from "./pages/Register";
 import "./styles/App.css";
 
 
-/*const admin = new Administrator();
-console.log('admin :>> ', admin);*/
+
 const App = () => {
 
   return (
@@ -21,7 +19,7 @@ const App = () => {
           <Route path="/register" element={<Register />}></Route>
           <Route path="/profile/:userName" element={<Profile />}></Route>
           <Route path="/editDetails/:userName" element={<EditDetails />}></Route>
-          <Route path="/admin/:userName" element={<Admin />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
         </Routes>
       </BrowserRouter>
     </UserContextProvider>
